a265c679e84c93ba2e0760b84376007eeb79d54d
Francois Korb <francois.korb@gmail.com>
2026-06-17 14:24:02 -0500

    Fix smoothie fruit API call and display response
    Updated the smoothie fruit nutrition information display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ if ingredients_list:
 
         
 # New section to display smoothiefruit nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
