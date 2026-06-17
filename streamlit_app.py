15e12db2a3936e2c761f849c157187e6297492db
Francois Korb <francois.korb@gmail.com>
2026-06-17 14:12:14 -0500

    Fix URL formatting for smoothiefruit API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ if ingredients_list:
         
 # New section to display smoothiefruit nutrition information
 import requests
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
